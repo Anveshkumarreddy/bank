36675b54287f3645ecada5c9d44e1b8551de2ba7
anveshkumarreddy <anveshkumarreddyinduri5@gmail.com>
2022-03-23 10:22:55 +0530

    Bank project

--- src/com/neo/bank/testcases/BaseClass.java
+++ src/com/neo/bank/testcases/BaseClass.java
@@ -1,26 +1,57 @@
 package com.neo.bank.testcases;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import com.neo.bank.utilities.ReadConfig;
 
 public class BaseClass 
 {
-	public String baseURL="https://demo.guru99.com/v4/";
-	public String username="mngr391553";
-	public String password="sAnanaj";
+	ReadConfig readconfig=new ReadConfig();
+	public String baseURL=readconfig.getApplicationURL();
+	public String username=readconfig.getUsername();
+	public String password=readconfig.getPassword();
 	public static WebDriver driver;
 
+	public static Logger logger;
 
+
+	@Parameters("browser")
 	@BeforeClass
-	public void setup()
+	public void setup(String br)
+
 	{
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
-		driver=new ChromeDriver();
+
+		logger=logger.getLogger("Bank");
+		PropertyConfigurator.configure("Log4j.properties");
+
+		if(br.equals("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver",readconfig.getChromePath());
+			driver=new ChromeDriver();
+		}
+		else if(br.equals("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver",readconfig.getFirefoxPath());
+			driver=new FirefoxDriver();
+		}
+		else if(br.equals("ie"))
+		{
+			System.setProperty("webdriver.ie.driver",readconfig.getIEPath());
+			driver=new InternetExplorerDriver();
+		}
+		driver.get(baseURL);
 	}
 	@AfterClass
 	public void tearDown()
+
 	{
 		driver.quit();   	
 	}
--- src/com/neo/bank/testcases/LoginTestCase.java
+++ src/com/neo/bank/testcases/LoginTestCase.java
@@ -1,6 +1,6 @@
 package com.neo.bank.testcases;
 
-import org.testng.Assert;
+import org.testng.Assert; 
 import org.testng.annotations.Test;
 
 import com.neo.bank.packageobjects.Loginpage;
@@ -10,19 +10,25 @@ public class LoginTestCase extends BaseClass
 	@Test
 	public void test()
 	{
-		driver.get(baseURL);
+		
+		logger.info("URL is opened");
 		Loginpage loginPage = new Loginpage(driver);
 		loginPage.setUsername(username);
+		logger.info("Entered username");
 		loginPage.setPassword(password);
+		logger.info("Entered password");
 		loginPage.clickSubmit();
+		logger.info("Clicked on Loginbtn");
 
 		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
 		{
 			Assert.assertTrue(true);
+			logger.info("Login test password");
 		}
 		else
 		{
 			Assert.assertTrue(false);
+			logger.info("Login test failed");
 		}
 	}
 }
